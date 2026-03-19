02708dcdbf6b83d58f44c8fc8606db50d2f250b0
mateuszwalkowiak-eit <mateusz.walkowiak.eit@gmail.com>
2026-03-19 14:54:32 +0100

    Create post-data.php

---
+++ meteo_backend/post-data.php
@@ -0,0 +1,52 @@
+<?php
+$servername = "localhost";
+$dbname = "name";
+$username = "user";
+$password = "pass";
+
+$api_key_value = "api";
+$api_key = $value1 = $value2 = $value3 = $value4 = $value5 = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $api_key = test_input($_POST["api_key"]);
+    if($api_key == $api_key_value) {
+        $value1 = test_input($_POST["value1"]);
+        $value2 = test_input($_POST["value2"]);
+        $value3 = test_input($_POST["value3"]);
+	$value4 = test_input($_POST["value4"]);
+	$value5 = test_input($_POST["value5"]);
+	$value6 = test_input($_POST["value6"]);
+        
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        
+        $sql = "INSERT INTO Sensor (value1, value2, value3, value4, value5, value6)
+        VALUES ('" . $value1 . "', '" . $value2 . "', '" . $value3 . "', '" . $value4 . "', '" . $value5 . "', '" . $value6 . "')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        }
+        else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    
+        $conn->close();
+    }
+    else {
+        echo "Wrong API Key provided.";
+    }
+
+}
+else {
+    echo "No data posted with HTTP POST.";
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
